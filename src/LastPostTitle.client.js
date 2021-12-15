7ff209e42737bf30bbd0f8b45217124df7841fc5
Michal Czaplinski <mmczaplinski@gmail.com>
2021-12-15 12:38:20 -0500

    Add an extra linebreak in the LastPostTitle

--- src/LastPostTitle.client.js
+++ src/LastPostTitle.client.js
@@ -5,13 +5,14 @@ import { useRefresh } from "./Cache.client";
 const LastPostTitle = ({ title }) => {
 	const refresh = useRefresh();
 	return (
-		<div>
+		<>
+			<br />
 			<div>The title of most recently updated post is:</div>
 			<div>
 				<b>{title}</b>
 			</div>
 			<button onClick={() => refresh()}> Reload </button>
-		</div>
+		</>
 	);
 };
 
